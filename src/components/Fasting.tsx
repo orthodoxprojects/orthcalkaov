1b35470005769efeb5cf9e9abf8777917046b23c
efraimkaov <efkaov@gmail.com>
2024-03-09 02:39:25 +0200

    Add Fasting icons

--- src/components/Fasting.tsx
+++ src/components/Fasting.tsx
@@ -1,5 +1,14 @@
 const Fasting = () => {
-  return <div>TI</div>;
+  return (
+    <div>
+      <img src="./img/totalFast.svg" alt="totalFast" />
+      <img src="./img/xerophagy.svg" alt="xerophagy" />
+      <img src="./img/wineAndOliveOil.svg" alt="wineAndOliveOil" />
+      <img src="./img/fishEating.svg" alt="fishEating" />
+      <img src="./img/dairyProducts.svg" alt="dairyProducts" />
+      <img src="./img/fastFree.svg" alt="fastFree" />
+    </div>
+  );
 };
 
 export default Fasting;
